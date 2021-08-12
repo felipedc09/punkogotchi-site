5fe2b594f1c7b60e9c5294a85be2f42988231ae5
Felipe Duitama <felipedc09@gmail.com>
2021-08-12 02:21:24 -0500

    UPDATE: clean index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import { FC, ReactElement } from 'react';
-import Footer from '../src/components/organisms/Footer/Footer';
-import Header from '../src/components/organisms/Header/Header';
-import Image from 'next/image';
+import Footer from '@/organisms/Footer/Footer';
+import Header from '@/organisms/Header/Header';
+import Window from '@/components/organisms/Window/Window';
 import styles from '../styles/Home.module.css';
 
-const App: FC = (): ReactElement => {
-  return (
-    <div className={styles.container}>
-      <Header />
-      <Image src="/static/images/mainTownBack.png" alt="main town" width={1000} height={1000} />
-      <Footer />
-    </div>
-  );
-};
+const App: FC = (): ReactElement => (
+  <div className={styles.container}>
+    <Header />
+    <Window imagePath="/static/images/homeBackground.png">hola</Window>
+    <Footer />
+  </div>
+);
 
 export default App;
