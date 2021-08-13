7ee30908d37509e9cb8066fef8ce88f9f460120b
Felipe Duitama <felipedc09@gmail.com>
2021-08-13 00:02:11 -0500

    UPDATE: use home component in App index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,6 @@
 import { FC, ReactElement } from 'react';
-import Footer from '@/organisms/Footer/Footer';
-import Header from '@/organisms/Header/Header';
-import Window from '@/components/organisms/Window/Window';
-import styles from '../styles/Home.module.css';
+import Home from './home/index';
 
-const App: FC = (): ReactElement => (
-  <div className={styles.container}>
-    <Header />
-    <Window imagePath="/static/images/homeBackground.png">hola</Window>
-    <Footer />
-  </div>
-);
+const App: FC = (): ReactElement => <Home />;
 
 export default App;
