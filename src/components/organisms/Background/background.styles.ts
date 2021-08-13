5b6699762f04e3eed3f962257857879cc9d1bb99
Felipe Duitama <felipedc09@gmail.com>
2021-08-13 00:10:16 -0500

    CREATE: background component

---
+++ src/components/organisms/Background/background.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #000;
+  color: #fff;
+`;
