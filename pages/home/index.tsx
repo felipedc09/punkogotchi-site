4e6aa3fff133bca3f43ee19a27a9650e003c7c92
Felipe Duitama <felipedc09@gmail.com>
2021-08-14 01:12:21 -0500

    CREATE: home template

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,20 +1,6 @@
 import { FC, ReactElement } from 'react';
-import Footer from '@/organisms/Footer/Footer';
-import Header from '@/organisms/Header/Header';
-import Window from '@/components/organisms/Window/Window';
-import Background from '@/components/organisms/Background/Background';
-import MediaBar from '@/components/organisms/MediaBar/MediaBar';
-import Subscribe from '@/components/organisms/Subscribe/Subscribe';
+import HomeTemplate from '@/components/templates/Home/HomeTemplate';
 
-const Home: FC = (): ReactElement => (
-  <Background>
-    <Header title="Home" />
-    <Window imagePath="/static/images/homeBackground.png">
-      <MediaBar />
-      <Subscribe />
-    </Window>
-    <Footer />
-  </Background>
-);
+const Home: FC = (): ReactElement => <HomeTemplate />;
 
 export default Home;
