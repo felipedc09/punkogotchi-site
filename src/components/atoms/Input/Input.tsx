45ac2b685ce366cea3f4667eef32099c9e471937
Felipe Duitama <felipedc09@gmail.com>
2021-08-13 23:56:21 -0500

    CREATE: add input styles

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,9 +1,6 @@
 import React, { FC, ReactElement } from 'react';
+import { NormalInput } from './input.styles';
 
-const Input: FC = (): ReactElement => (
-  <div>
-    <input type="text" />
-  </div>
-);
+const Input: FC = (): ReactElement => <NormalInput placeholder="Your email address" />;
 
 export default Input;
