170b4f594fb074279895b95e6611b1cd0137f416
Felipe Duitama <felipedc09@gmail.com>
2021-08-13 23:55:58 -0500

    CREATE: types theme styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement } from 'react';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styles/Theme';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
+export default MyApp;
