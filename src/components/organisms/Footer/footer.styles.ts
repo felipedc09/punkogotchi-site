2d6fa5642cc34e720471ec9e1627b52bca9a4f0b
Felipe Duitama <felipedc09@gmail.com>
2021-08-16 22:43:28 -0500

    UPDATE: fix styles

--- src/components/organisms/Footer/footer.styles.ts
+++ src/components/organisms/Footer/footer.styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const CopyRight = styled.footer`
-    position: absolute;
-    bottom: 10px;
+    padding: 20px;
     font-size: 24px;
+    color: #fff;
 `;
