107431d3af8c9936b01bdc82b36b44ad2d2cefe6
Felipe Duitama <felipedc09@gmail.com>
2021-08-17 23:15:35 -0500

    ADD: tickets page

---
+++ pages/tickets/index.tsx
@@ -0,0 +1,6 @@
+import { FC, ReactElement } from 'react';
+import MainTemplate from '@/components/templates/Main/MainTemplate';
+
+const Tickets: FC = (): ReactElement => <MainTemplate page="Tickets" />;
+
+export default Tickets;
