a9e3c2b8531d11fe421d5be31cff9561b819ac2b
Felipe Duitama <felipedc09@gmail.com>
2021-08-18 00:38:45 -0500

    UPDATE: assign styles to backgropund images

--- src/components/organisms/Background/background.styles.ts
+++ src/components/organisms/Background/background.styles.ts
@@ -1,6 +1,28 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+type ContainerProps={
+  skyColor: string
+}
 
-  background-color: #000;
+type ImageProps={
+  position?: string
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: ${(props) => props.skyColor};
+  background: linear-gradient(180deg, ${(props) => props.skyColor} 50%, rgba(156,139,219,1) 50%);
+`;
+
+export const Image = styled.img<ImageProps>`
+  position:absolute;
+  bottom: ${(props) => (props.position ? props.position : 20)}%;
+  width: 100%;
+  `;
+
+export const FrontImage = styled.div<ImageProps>`
+  position:absolute;
+  background: url("/static/images/frontBackground.svg") no-repeat top left;
+  background-size: contain;
+  width: 100%;
+  height: 100%;
 `;
