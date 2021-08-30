a781d12bda6ccace38c47c33b885e3bc4ece75f4
Felipe Duitama <felipedc09@gmail.com>
2021-08-30 01:51:29 -0500

    ADD: create step button

---
+++ src/components/molecules/StepButton/StepButton.tsx
@@ -0,0 +1,45 @@
+import React, { Dispatch, FC, ReactElement, SetStateAction } from 'react';
+import { Button } from '@/components/atoms/IconButton/iconButton.styles';
+import { StepImage } from './stepButton.styles';
+
+export type Step = {
+  name: string;
+  status: 'done' | 'progress' | 'to do';
+  image: string;
+};
+
+type Props = {
+  step: Step;
+  position: number;
+  onClick: Dispatch<SetStateAction<string>>;
+};
+
+const StepButton: FC<Props> = (props): ReactElement => {
+  const { step, position, onClick } = props;
+
+  function selectLight(): string {
+    switch (step.status) {
+      case 'done':
+        return 'lightGreen';
+      case 'progress':
+        return 'lightYellow';
+      case 'to do':
+        return 'lightOff';
+      default:
+        return 'lightYellow';
+    }
+  }
+
+  return (
+    <Button
+      onClick={() => {
+        onClick(step.image);
+      }}
+      type="button"
+    >
+      <StepImage src={`/static/images/roadmap/${selectLight()}.png`} alt="light" top={position.toString()} />
+    </Button>
+  );
+};
+
+export default StepButton;
