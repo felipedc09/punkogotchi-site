1fca3c98876621ed38b040f776c815805a414923
Felipe Duitama <felipedc09@gmail.com>
2021-09-01 22:54:55 -0500

    ADD: creat checkbox component

---
+++ src/components/atoms/Checkbox/Checkbox.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactElement } from 'react';
+import { CheckboxContainer, HiddenCheckbox, Icon, StyledCheckbox } from './checkbox.styles';
+
+type Props = {
+  checked: boolean;
+  color: string;
+};
+
+const Checkbox: FC<Props> = ({ checked, color }): ReactElement => (
+  <CheckboxContainer>
+    <HiddenCheckbox checked={checked} />
+    <StyledCheckbox color={color} checked={checked}>
+      <Icon viewBox="0 0 24 24">
+        <polyline points="20 6 9 17 4 12" />
+      </Icon>
+    </StyledCheckbox>
+  </CheckboxContainer>
+);
+
+export default Checkbox;
