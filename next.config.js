44765d68c0db10a76d16859dfe1783e4a785be8a
Felipe Duitama <felipedc09@gmail.com>
2021-09-12 07:27:28 -0500

    UPDATE: fix load styles

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
-module.exports = {
+const withOffline = require('next-offline');
+
+module.exports = withOffline({
   reactStrictMode: true,
-};
+  devIndicators: {
+    autoPrerender: false,
+  },
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? 'service-worker.js'
+      : 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+});
