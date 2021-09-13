9e099bb6974d4e6f1066679c63862d1fb1c7469c
Felipe Duitama <felipedc09@gmail.com>
2021-09-12 22:45:50 -0500

    UPDATE: update conf files

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
-const withOffline = require('next-offline');
 
-module.exports = withOffline({
+module.exports = {
   reactStrictMode: true,
-  devIndicators: {
-    autoPrerender: false,
-  },
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-});
+};
