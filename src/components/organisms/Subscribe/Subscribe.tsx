9322e367c14a47cfdf392085dbb52faf054d0f8d
Felipe Duitama <felipedc09@gmail.com>
2021-09-19 21:46:39 -0500

    UPDATE: add animation to subscribe button

--- src/components/organisms/Subscribe/Subscribe.tsx
+++ src/components/organisms/Subscribe/Subscribe.tsx
@@ -1,14 +1,16 @@
 import { FC, ReactElement } from 'react';
 import Button from '@/components/atoms/Button/Button';
 import Input from '@/components/atoms/Input/Input';
-import { Form } from './subscribe.styles';
+import { Form, Animation } from './subscribe.styles';
 
 const Subscribe: FC = (): ReactElement => {
   const color = '#9052BC';
   return (
     <Form color={color}>
       <Input />
-      <Button text="Subscribe" iconPath="/static/icons/bell.svg" />
+      <Animation>
+        <Button text="Subscribe" iconPath="/static/icons/bell.svg" />
+      </Animation>
     </Form>
   );
 };
--- src/components/organisms/Subscribe/subscribe.styles.ts
+++ src/components/organisms/Subscribe/subscribe.styles.ts
@@ -16,3 +16,22 @@ export const Form = styled.form<FormProps>`
     width: 380px;
     height: 40px;
 `;
+
+export const Animation = styled.div`
+    &:hover, &:focus {
+        animation: bellshake .5s cubic-bezier(.36,.07,.19,.97) both;
+        backface-visibility: hidden;
+        transform-origin: top right;
+  }
+  @keyframes bellshake {
+  0% { transform: rotate(0); }
+  15% { transform: rotate(5deg); }
+  30% { transform: rotate(-5deg); }
+  45% { transform: rotate(4deg); }
+  60% { transform: rotate(-4deg); }
+  75% { transform: rotate(2deg); }
+  85% { transform: rotate(-2deg); }
+  92% { transform: rotate(1deg); }
+  100% { transform: rotate(0); }
+}
+`;
