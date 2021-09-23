7838e3ca200eca8d70ff69a6d458917a70ae225a
Felipe Duitama <felipedc09@gmail.com>
2021-09-23 00:34:37 -0500

    UPDATE: next custom document

--- pages/_docuent.tsx
+++ pages/_docuent.tsx
@@ -1,21 +1,15 @@
 /* eslint-disable react/display-name */
 /* eslint-disable @next/next/no-document-import-in-page */
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-  
-  displayName = 'document';
-
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-        });
+      ctx.renderPage = () => originalRenderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
 
       const initialProps = await Document.getInitialProps(ctx);
       return {
@@ -24,6 +18,7 @@ class MyDocument extends Document {
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+            <NextScript />
           </>
         ),
       };
@@ -32,6 +27,5 @@ class MyDocument extends Document {
     }
   }
 }
-
 
 export default MyDocument;
