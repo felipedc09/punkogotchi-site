4c7ad7de9ae2e8b32bb2de4b4a4227df87ecda4b
agar3s <ekeisco@gmail.com>
2021-10-12 16:06:02 -0500

    subscribe connected to mailchimp - missing feedback

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,12 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import mailchimp, { Status } from '@mailchimp/mailchimp_marketing';
 
-type Data = {
-  name: string
-}
+mailchimp.setConfig({
+  apiKey: 'cbdcfcb92fc46084d1b3b7949dffbec5-us5',
+  server: 'us5',
+});
 
 export default function handler(
-  res: NextApiResponse<Data>,
+  req: NextApiRequest,
+  res: NextApiResponse,
 ): void {
-  res.status(200).json({ name: 'John Doe' });
+  const { email } = req.body;
+  if (!email) {
+    res.status(400).json({ error: 'Email is required' });
+    return;
+  }
+  mailchimp.lists.addListMember('d1cca5a05d', {
+    email_address: email,
+    status: Status?.subscribed || 'subscribed'
+  }).then(() => {
+    res.status(201).json({ error: '' });
+  }).catch((error) => {
+    res.status(500).json({ error: error.message || error.toString() });
+  });
 }
--- src/components/organisms/Subscribe/Subscribe.tsx
+++ src/components/organisms/Subscribe/Subscribe.tsx
@@ -3,23 +3,27 @@ import { NormalButton } from '@/components/atoms/Button/button.styles';
 import { NormalInput } from '@/components/atoms/Input/input.styles';
 import { Form, Animation } from './subscribe.styles';
 
-const mailchimp = require('@mailchimp/mailchimp_marketing');
-
-mailchimp.setConfig({
-  apiKey: "cbdcfcb92fc46084d1b3b7949dffbec5-us5",
-  server: "us5",
-});
-
 
 const Subscribe: FC = (): ReactElement => {
   const color = '#9052BC';
   const onClick = async () => {
     const inputEmail = document.getElementById('email') as HTMLInputElement;
-    const response = await mailchimp.lists.addListMember('d1cca5a05d', {
-      email_address: inputEmail.value,
-      status: 'pending',
+    const res = await fetch('/api/hello', {
+      body: JSON.stringify({
+        email: inputEmail.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
     });
-    console.log(response)
+
+    const { error } = await res.json();
+
+    if (error) {
+      return;
+    }
+    inputEmail.value = '';
   }
   return (
     <Form color={color}>
