faab5ecfb933741aaf0b8903276c9f5d0b6bd1fc
agar3s <ekeisco@gmail.com>
2021-10-12 13:27:19 -0500

    mailchimp integration

--- src/components/organisms/Subscribe/Subscribe.tsx
+++ src/components/organisms/Subscribe/Subscribe.tsx
@@ -1,16 +1,29 @@
 import { FC, ReactElement } from 'react';
 import { NormalButton } from '@/components/atoms/Button/button.styles';
-import Input from '@/components/atoms/Input/Input';
+import { NormalInput } from '@/components/atoms/Input/input.styles';
 import { Form, Animation } from './subscribe.styles';
+
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+
+mailchimp.setConfig({
+  apiKey: "cbdcfcb92fc46084d1b3b7949dffbec5-us5",
+  server: "us5",
+});
+
 
 const Subscribe: FC = (): ReactElement => {
   const color = '#9052BC';
-  const onClick = () => {
-    console.log('do something');
+  const onClick = async () => {
+    const inputEmail = document.getElementById('email') as HTMLInputElement;
+    const response = await mailchimp.lists.addListMember('d1cca5a05d', {
+      email_address: inputEmail.value,
+      status: 'pending',
+    });
+    console.log(response)
   }
   return (
     <Form color={color}>
-      <Input />
+      <NormalInput id='email' placeholder="Your email address"/>
       <Animation>
         <NormalButton onClick={onClick} type='button'>
           subscribe
